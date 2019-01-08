d0106da2cfd645c869b291d7171ae75feb0cda7c
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2019-01-08 13:34:04 +0700

    Create ajukan.php

---
+++ ajukan.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cuti Pegawai</title>
+        <link rel="stylesheet" type="text/css" href="css/styleajukan.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato:700" rel="stylesheet"> 
+        <script
+            src="https://code.jquery.com/jquery-3.3.1.js"
+            integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+            crossorigin="anonymous">
+        </script>
+    </head>
+    <body class="sec1">
+        <div class="divbackground"></div>
+        <div>
+        <nav>
+            <div class="logo">
+                LOGO
+            </div>
+            <div>
+            <ul class="ul1">
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="ajukan.php">Ajukan Cuti</a>
+                    </li>
+                    <li>
+                        <a href="dataanda.php">Data Anda</a>
+                    </li>
+                    <li>
+                        <a href="about.php">About</a>
+                    </li>
+                </ul>
+                <ul class="ul2">
+                    <li>
+                        <a href="login.php" class="active">Login</a>
+                    </li>
+                    <li>
+                        <a href="register.php">Register</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        </div>
+        <div class="div1">
+            Hey
+        </div>
+    </body>
+</html>
